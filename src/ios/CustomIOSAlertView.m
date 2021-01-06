90a1614d15da6dc33f0c4b9b2ae442478fe5de5d
Vason <henpuri@qq.com>
2021-01-06 17:19:17 +0800

    update dialog style

--- src/ios/CustomIOSAlertView.m
+++ src/ios/CustomIOSAlertView.m
@@ -214,18 +214,18 @@ CGFloat buttonSpacerHeight = 0;
     UIView *dialogContainer = [[UIView alloc] initWithFrame:CGRectMake(0, 0, dialogSize.width, dialogSize.height)];
     
     // First, we style the dialog to match the iOS7 UIAlertView >>>
-    CAGradientLayer *gradient = [CAGradientLayer layer];
-    gradient.frame = dialogContainer.bounds;
-    gradient.colors = [NSArray arrayWithObjects:
-                       (id)[[UIColor colorWithRed:218.0/255.0 green:218.0/255.0 blue:218.0/255.0 alpha:1.0f] CGColor],
-                       (id)[[UIColor colorWithRed:233.0/255.0 green:233.0/255.0 blue:233.0/255.0 alpha:1.0f] CGColor],
-                       (id)[[UIColor colorWithRed:218.0/255.0 green:218.0/255.0 blue:218.0/255.0 alpha:1.0f] CGColor],
-                       nil];
-    
+//    CAGradientLayer *gradient = [CAGradientLayer layer];
+//    gradient.frame = dialogContainer.bounds;
+//    gradient.colors = [NSArray arrayWithObjects:
+//                       (id)[[UIColor colorWithRed:218.0/255.0 green:218.0/255.0 blue:218.0/255.0 alpha:1.0f] CGColor],
+//                       (id)[[UIColor colorWithRed:233.0/255.0 green:233.0/255.0 blue:233.0/255.0 alpha:1.0f] CGColor],
+//                       (id)[[UIColor colorWithRed:218.0/255.0 green:218.0/255.0 blue:218.0/255.0 alpha:1.0f] CGColor],
+//                       nil];
+//
+//
+//    gradient.cornerRadius = cornerRadius;
+//    [dialogContainer.layer insertSublayer:gradient atIndex:0];
     CGFloat cornerRadius = kCustomIOSAlertViewCornerRadius;
-    gradient.cornerRadius = cornerRadius;
-    [dialogContainer.layer insertSublayer:gradient atIndex:0];
-    
     dialogContainer.layer.cornerRadius = cornerRadius;
     dialogContainer.layer.borderColor = [[UIColor colorWithRed:198.0/255.0 green:198.0/255.0 blue:198.0/255.0 alpha:1.0f] CGColor];
     dialogContainer.layer.borderWidth = 0;
--- src/ios/openWebview.m
+++ src/ios/openWebview.m
@@ -5,6 +5,8 @@
 #import <Foundation/Foundation.h>
 #import <WebKit/WebKit.h>
 #import "CustomIOSAlertView.h"
+#import "AppDelegate.h"
+#import "OpenWebvViewController.h"
 
 
 @interface openWebview : CDVPlugin {
@@ -12,6 +14,8 @@
 }
 
 @property (nonatomic, strong) NSMutableArray *alertViewList;
+@property (nonatomic, strong) NSMutableArray *webvieViewList;
+@property (nonatomic, assign) BOOL isSubWebView;
 
 - (void)open:(CDVInvokedUrlCommand*)command;
 
@@ -27,7 +31,11 @@
 
     if (options != nil) {
         NSString *redirectUrl = options[@"url"];
-        NSLog(@"redirectUrl: %@", redirectUrl);
+        NSString *fromWhere = options[@"fromWhere"];
+        if ([fromWhere isEqual:@"webview"]) {
+            NSLog(@"open fromWhere: %@", fromWhere);
+            self.isSubWebView = YES;
+        }
         int alertViewListCount = (int)[self.alertViewList count];
         if(alertViewListCount == 2){
             return;
@@ -35,13 +43,17 @@
         if ([redirectUrl hasPrefix:@"http://"] || [redirectUrl hasPrefix:@"https://"]) {
             CustomIOSAlertView *alertView = [[CustomIOSAlertView alloc] init];
             UIView *outContainer = [[UIView alloc] initWithFrame:CGRectMake(0, 0, [UIScreen mainScreen].bounds.size.width, [UIScreen mainScreen].bounds.size.height)];
+            UIColor *color = [UIColor blackColor];
+            outContainer.backgroundColor = [color colorWithAlphaComponent:0.3f];
+//            outContainer.backgroundColor = [UIColor blueColor];
             if (alertViewListCount == 0) {
                 self.alertViewList = [[NSMutableArray alloc]init];
             }
             [self.alertViewList addObject:alertView];
-            
             UIView *actionBar = [self createActionBar];
             WKWebView *aWebView = [self createWebView];
+            // webview 标识：5600
+            aWebView.tag = 5600 + [self.alertViewList count];
             // 设置访问的URL
             NSURL *url = [NSURL URLWithString:redirectUrl];
             // 根据URL创建请求
@@ -71,15 +83,40 @@
 -(UIView *)createActionBar
 {
     // This is the dialog's container; we attach the custom content and the buttons to this one
-    UIView *actionContainer = [[UIView alloc] initWithFrame:CGRectMake(0, 0, [UIScreen mainScreen].bounds.size.width, [self getStatusBarHeight] + 44)];
+    int pos_y = 0;
+    if(self.isSubWebView){
+        // 从 webview 中 再打开 webview，减少高度
+        pos_y = 80;
+    }
+    UIView *actionContainer = [[UIView alloc] initWithFrame:CGRectMake(0, pos_y, [UIScreen mainScreen].bounds.size.width, [self getStatusBarHeight] + 44)];
     actionContainer.layer.backgroundColor = [[UIColor colorWithRed:211.0/255.0 green:17.0/255.0 blue:69.0/255.0 alpha:1.0f] CGColor];
     
+    if(self.isSubWebView){
+        // 从 webview 中 再打开 webview，设置圆角
+        CGFloat radius = 10; // 圆角大小
+        UIRectCorner corner = UIRectCornerTopLeft | UIRectCornerTopRight; // 圆角位置
+        UIBezierPath * path = [UIBezierPath bezierPathWithRoundedRect:actionContainer.bounds byRoundingCorners:corner cornerRadii:CGSizeMake(radius, radius)];
+        CAShapeLayer *maskLayer = [[CAShapeLayer alloc] init];
+        maskLayer.frame = actionContainer.bounds;
+        maskLayer.path = path.CGPath;
+        actionContainer.layer.mask = maskLayer;
+    }
+    
+    // close button
     UIButton *closeButton = [UIButton buttonWithType:UIButtonTypeCustom];
-    closeButton.frame = CGRectMake(0, [self getStatusBarHeight], 80, 40);
-    [closeButton setTitle:@"CLOSE" forState:UIControlStateNormal];
+    closeButton.frame = CGRectMake(10, [self getStatusBarHeight] + 5, 30, 30);
+    [closeButton setImage:[UIImage imageWithContentsOfFile:[NSString stringWithFormat:@"%@/%@", [[NSBundle mainBundle] bundlePath],@"open_webview_close@3x.png"]] forState:UIControlStateNormal];
     [closeButton setTitleColor:[UIColor colorWithRed:1.0 green:1.0 blue:1.0 alpha:1.0] forState:UIControlStateNormal];
     [closeButton addTarget:self action:@selector(closeDialog:) forControlEvents:UIControlEventTouchUpInside];
     [actionContainer addSubview:closeButton];
+    
+    // back button
+    UIButton *backButton = [UIButton buttonWithType:UIButtonTypeCustom];
+    backButton.frame = CGRectMake(40, [self getStatusBarHeight] + 5, 30, 30);
+    [backButton setImage:[UIImage imageWithContentsOfFile:[NSString stringWithFormat:@"%@/%@", [[NSBundle mainBundle] bundlePath],@"open_webview_back@3x.png"]] forState:UIControlStateNormal];
+    [backButton setTitleColor:[UIColor colorWithRed:1.0 green:1.0 blue:1.0 alpha:1.0] forState:UIControlStateNormal];
+    [backButton addTarget:self action:@selector(webviewBack:) forControlEvents:UIControlEventTouchUpInside];
+//    [actionContainer addSubview:backButton];
     
     return actionContainer;
 }
@@ -98,22 +135,42 @@
     }
 }
 
+// webview 后退
+- (void)webviewBack:(UIButton *)btn
+{
+    int alertViewListCount = (int)[self.alertViewList count];
+    if(alertViewListCount > 0){
+        CustomIOSAlertView *getAlertView = [self.alertViewList lastObject];
+        NSLog(@"closeDialog getAlertView: %@", getAlertView);
+        [getAlertView close];
+        [self.alertViewList removeLastObject];
+    }else{
+        return;
+    }
+}
+
 
 -(WKWebView *)createWebView
 {
+    int pos_y = 44;
+    if(self.isSubWebView){
+        pos_y = 44 + 80;
+    }
     WKWebViewConfiguration *configuration = [[WKWebViewConfiguration alloc] init];
     //实例化对象
     configuration.userContentController = [WKUserContentController new];
     //调用JS方法
     [configuration.userContentController addScriptMessageHandler:self name:@"openNew"];
     // 创建WKWebView
-    WKWebView *webView = [[WKWebView alloc] initWithFrame:CGRectMake(0, [self getStatusBarHeight] + 44, [UIScreen mainScreen].bounds.size.width, [UIScreen mainScreen].bounds.size.height - [self getStatusBarHeight] - 44) configuration:configuration];
+    WKWebView *webView = [[WKWebView alloc] initWithFrame:CGRectMake(0, [self getStatusBarHeight] + pos_y, [UIScreen mainScreen].bounds.size.width, [UIScreen mainScreen].bounds.size.height - [self getStatusBarHeight] - pos_y) configuration:configuration];
+    webView.navigationDelegate = self;
     return webView;
 }
 
 #pragma mark - WKScriptMessageHandler
 - (void)userContentController:(WKUserContentController *)userContentController didReceiveScriptMessage:(WKScriptMessage *)message
 {//    message.body  --  Allowed types are NSNumber, NSString, NSDate, NSArray,NSDictionary, and NSNull.
+    // [[UIApplication sharedApplication].delegate.window.rootViewController presentViewController:(nonnull UIViewController *) animated:YES completion:nil]
     if(message.body == nil){
         return;
     }
@@ -122,7 +179,8 @@
         NSLog(@"openNew alertViewList: %@", [self alertViewList]);
         CustomIOSAlertView *getAlertView = [self alertViewList].lastObject;
         NSLog(@"openNew getAlertView: %@", getAlertView);
-        NSString *jsStr = [NSString stringWithFormat:@"cordova.plugins.openWebview.open({url:'%@'})",msgData[@"url"]];
+        NSString *openFromWhere = @"webview";
+        NSString *jsStr = [NSString stringWithFormat:@"cordova.plugins.openWebview.open({url:'%@', fromWhere:'%@'})",msgData[@"url"], openFromWhere];
         if(self.webViewEngine){
             [self.webViewEngine evaluateJavaScript:jsStr completionHandler:^(id object, NSError * _Nullable error) {
                 NSLog(@"obj:%@---error:%@", object, error);
@@ -130,6 +188,27 @@
         }
     }
 }
+
+#pragma mark - WKNavigationDelegate
+// 页面开始加载时调用
+- (void)webView:(WKWebView *)webView didStartProvisionalNavigation:(WKNavigation *)navigation{
+    NSLog(@"WKNavigationDelegate canGoBack: %@", webView.canGoBack? @"YES": @"NO");
+    NSLog(@"WKNavigationDelegate Tag: %i", webView.tag);
+    NSLog(@"WKNavigationDelegate didStartProvisionalNavigation: %@", @"didStartProvisionalNavigation");
+}
+// 当内容开始返回时调用
+- (void)webView:(WKWebView *)webView didCommitNavigation:(WKNavigation *)navigation{
+    NSLog(@"WKNavigationDelegate didCommitNavigation: %@", @"didCommitNavigation");
+}
+// 页面加载完成之后调用
+- (void)webView:(WKWebView *)webView didFinishNavigation:(WKNavigation *)navigation{
+    NSLog(@"WKNavigationDelegate didFinishNavigation: %@", @"didFinishNavigation");
+}
+// 页面加载失败时调用
+- (void)webView:(WKWebView *)webView didFailProvisionalNavigation:(WKNavigation *)navigation{
+    NSLog(@"WKNavigationDelegate didFailProvisionalNavigation: %@", @"didFailProvisionalNavigation");
+}
+
 
 // 递归获取子视图
 - (UIView *)viewWithTagNotCountingSelf:(NSInteger)tag
@@ -144,6 +223,12 @@
         }
     }
     return toReturn;
+}
+
++ (UIImage*)imageFromMainBundleFile_zmm:(NSString*)aFileName
+{
+    NSString* bundlePath = [[NSBundle mainBundle] bundlePath];
+    return [UIImage imageWithContentsOfFile:[NSString stringWithFormat:@"%@/%@", bundlePath,aFileName]];
 }
 
 @end
